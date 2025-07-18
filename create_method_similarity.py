4a8603cd5749e4be5140226e1f7c65c7980722fe
alexwoolford <alex@woolford.io>
2025-07-18 16:45:55 -0600

    Using Cypher projection for GDS

--- create_method_similarity.py
+++ create_method_similarity.py
@@ -107,10 +107,10 @@ def run_knn(gds, top_k=5, cutoff=0.8):
     if exists:
         gds.graph.drop(graph_name)
 
-    graph, _ = gds.graph.project(
+    graph, _ = gds.graph.project.cypher(
         graph_name,
-        {"Method": {"properties": "embedding", "where": "m.embedding IS NOT NULL"}},
-        "*",
+        "MATCH (m:Method) WHERE m.embedding IS NOT NULL RETURN id(m) AS id, m.embedding AS embedding",
+        "RETURN null AS source, null AS target LIMIT 0"
     )
 
     start = perf_counter()
